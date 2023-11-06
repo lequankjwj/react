0d97ab080d279764adca9abdcb8a7c687117d8fd
qitle <lequankjwjq2909@gmail.com>
2023-11-06 14:18:26 +0700

    hh

--- src/containers/Career/index.tsx
+++ src/containers/Career/index.tsx
@@ -1,7 +1,7 @@
 export default function Career() {
   return (
     <div
-      style={{ height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      style={{ height: '82vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
     >
       <h2 style={{ fontSize: '30px' }}>Career Page</h2>
     </div>
